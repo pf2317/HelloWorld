9258db6435c5e9ee850e59b5cad305e69888bb0b
pf2317 <pf2317@columbia.edu>
2022-07-18 23:58:38 +0800

    add a file

---
+++ test.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+test = 7
+print(test)
+print(test)
+print(test)
+
+
+def main():
+    hello = 0
+    hello = 0
+    print(hello)
+
+
+run = True
+
+try:
+    run
+except:
+    pass
